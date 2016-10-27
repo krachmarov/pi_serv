71dd99f042fb01f9a53437b5351753c58ad77ee2
Georgi Krachmarov <gkrachmarov@mm-sol.com>
2016-10-27 17:19:53 +0300

    temp: some socket reading
    Signed-off-by: Georgi Krachmarov <georgi.krachmarov@gmail.com>

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -10,6 +12,13 @@
 #define SERVER_PORT 7812
 
 #define THREAD_REQUEST_EXIT 1
+#define CLIENT_CON_CHECK_TIME    3
+#define CLIENT_NOT_VERIFIED_TO_S 10
+
+typedef enum {
+    CLIENT_CONNECTED,
+    CLIENT_VERIFIED,
+}client_cond_state_t;
 
 typedef struct {
     int sockfd;
@@ -17,20 +26,72 @@ typedef struct {
     int newsockfd;
     int port;
     struct sockaddr_in addr;
+
 } server_info_t;
 
 typedef struct {
+    int socket_fd;
+    pthread_t wd_thread;
     pthread_t thread;
     uint32_t spawn_id;
     char target_machine_user[30];
     char target_machine_password[30];
     char target_ip[15];
+    bool client_exit;
+    uint8_t not_verified_timeout;
     server_info_t *server_info;
+    client_cond_state_t state;
 } client_info_t;
+
+static void load_client_defaults(client_info_t *client_info,
+    server_info_t *server_info,
+    int socket_fd)
+{
+    client_info->server_info = server_info;
+    client_info->socket_fd = socket_fd;
+    client_info->client_exit = false;
+    client_info->state = CLIENT_CONNECTED;
+    client_info->not_verified_timeout = 0;
+}
+/** thr_client_wd
+ *
+ * Description:
+ *          Watch dog thread for client thread
+ *
+ */
+void *thr_client_wd(void *context)
+{
+    int con_check_time = 0;
+    client_info_t *client_info = context;
+
+    while (client_info->client_exit == false) {
+        sleep(1);
+        switch (client_info->state) {
+            case CLIENT_CONNECTED:
+                if (client_info->not_verified_timeout < CLIENT_NOT_VERIFIED_TO_S) {
+                    client_info->not_verified_timeout ++;
+                } else {
+                    printf("CLIENT_CONNECTED timed out\n");
+                    client_info->client_exit = true;
+                }
+                break;
+            case CLIENT_VERIFIED:
+                con_check_time ++;
+                if (con_check_time < CLIENT_CON_CHECK_TIME) {
+                    printf("Check client connectivity\n");
+                    con_check_time = 0;
+                }
+                break;
+        }
+    }
+    return NULL;
+}
 
 /* clients will spawn this thread*/
 void *thr_client(void *context)
 {
+    uint8_t buffer[255];
+    int n;
     client_info_t *client_info = context;
 
     if (!client_info) {
@@ -38,15 +99,25 @@ void *thr_client(void *context)
         return NULL;
     }
 
+    pthread_create(&client_info->wd_thread,
+        NULL, &thr_client_wd, (void *)client_info);
+
+    while (client_info->client_exit == false) {
+       n = read(client_info->socket_fd, buffer, 5);
+       if (n = 5) {
+
+       }
+    }
+
     printf("[%s: %d] client thread created\n", __func__, __LINE__);
     printf("[%s: %d] Now try to exit\n", __func__, __LINE__);
 
+    pthread_join(client_info->wd_thread, NULL);
     client_info->server_info->exit_req = 1;
     return NULL;
 }
 
-/* server spawn this thread, only
-one instance */
+/* server spawn this thread, only one instance */
 void *my_server(void *args)
 {
     int rc;
@@ -93,9 +164,10 @@ void *my_server(void *args)
         if(socketfd) {
             printf("[%s: %d] Client Connected.Try to create client thread\n", __func__, __LINE__);
             client_info = NULL;
+            /*TODO: Add client_info in lists */
             client_info = (client_info_t*)malloc(sizeof(client_info_t));
             if (client_info) {
-                client_info->server_info = server_info;
+                load_client_defaults(client_info, server_info, socketfd);
                 pthread_create(&client_info->thread,
                     NULL, &thr_client, (void *)client_info);
             }
